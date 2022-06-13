6242cfc9830da0e38ddd420e1b6f52f505d0163a
icoolww <williamwijaya.mail@gmail.com>
2022-06-13 23:16:36 +0000

    Adding axios

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,23 @@
 import { storage } from "./firebase";
+import React, { Component } from 'react';
+import { useState, useEffect } from "react";
 import './App.css';
 import Header from "./components/Header";
+import axios from "axios";
+
 
 
 function App() {
+
+  useEffect(() => {
+    axios.get(`/api`).then(
+      (res) => {
+        console.log("response", res)
+      }
+    )
+  }, []);
+
+
   return (
     <div className="bg-oatmeal App">
       
@@ -11,5 +25,7 @@ function App() {
     </div>
   );
 }
+
+
 
 export default App;
