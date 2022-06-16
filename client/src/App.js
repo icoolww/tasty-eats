4c18f4dd09acce0dd9345b1980ba59b6df61d16a
icoolww <williamwijaya.mail@gmail.com>
2022-06-16 05:27:32 +0000

    Adding single recipe page

--- client/src/App.js
+++ client/src/App.js
@@ -1,93 +1,84 @@
 import { storage } from "./firebase";
 import axios from "axios";
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { useState, useEffect } from "react";
-import {BrowserRouter, Link} from "react-router-dom"
-import './App.css';
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import "./App.css";
 import Header from "./components/Header";
 import RecipePreview from "./components/RecipePreview";
 import CreateRecipe from "./components/CreateRecipe";
 import Breakfast from "./components/Breakfast";
 import Sidebar from "./components/Sidebar";
 import SearchRecipe from "./components/SearchRecipe";
+import RecipeCard from "./components/RecipeCard";
 
 function App() {
-
   const [state, setState] = useState([]);
-  const [searchValue, setSearchValue] = useState('');
+  const [searchValue, setSearchValue] = useState("");
+  const [selectedRecipe, setSelectedRecipe] = useState(null);
 
   console.log("state", state);
   // list of all recipe
 
-
   useEffect(() => {
     axios
-    // .get(`/api`).then(
-      .get(`/api`).then((res)=>{
+      // .get(`/api`).then(
+      .get(`/api`)
+      .then((res) => {
         const data = res.data;
-      console.log('data', data);
-      setState(data);
-    })
-
+        console.log("data", data);
+        setState(data);
+      });
   }, []);
 
   const filterRecipe = () => {
-    if (searchValue === '') return state;
-    return state.filter((recipe) => recipe.title.toLowerCase().includes(searchValue.toLowerCase()))
-  }
+    if (searchValue === "") return state;
+    return state.filter((recipe) =>
+      recipe.title.toLowerCase().includes(searchValue.toLowerCase())
+    );
+  };
 
   const filteredRecipe = filterRecipe();
 
-  // const updateRecipe = 
+  // const updateRecipe =
   // call server endpoint to get the data with search value
 
   // console.log("title", a.title)
   // console.log("time", recipe.prep_time)
 
-return (
+  // if (selectedRecipe) {
+  //   return <RecipeCard recipe={selectedRecipe} />
+  // }
+
+  return (
     <div className="bg-oatmeal App">
       <Header onSearchValueChanged={setSearchValue} />
-      
 
-    <div className="flex flex-wrap">
-    <Sidebar />
-      {filteredRecipe.map((recipe) => (
-        // <div>{recipe.title} - {recipe.prep_time} - {recipe.portion_size}</div>
-        <RecipePreview recipe={recipe} />
-      ))}
+      <div className="flex flex-wrap">
+        <Sidebar />
 
-        {/* <BrowserRouter>
-          <nav>
-            <Link to="/about">About</Link><br/>
-            <Link to="/">Home</Link><br/>
-            <Link to="/products">Products</Link><br/>
-          </nav>
-        </BrowserRouter> */}
+        {selectedRecipe ? <RecipeCard recipe={selectedRecipe} /> : filteredRecipe.map((recipe) => (
+          // <div>{recipe.title} - {recipe.prep_time} - {recipe.portion_size}</div>
+          <RecipePreview recipe={recipe} onRecipeClick={setSelectedRecipe} />
+          // onClick={props.onClick}
+        ))}
+
+          {/* <nav>
+            <Link to="/about">About</Link>
+            <br />
+            <Link to="/">Home</Link>
+            <br />
+            <Link to="/recipe">Recipe</Link>
+            <br />
+            <Link to="/favorites">Favorites</Link>
+            <br />
+          </nav> */}
 
 
-        {/* <div className="flex flex-wrap">
-      {state.filter((val) => {
-        console.log("val", val)
-        // list the recipes in object
-        if (search == "") {
-          return val;
-        } else if (val.title.toLowerCase().incudes(search.toLowerCase())) {
-          return val;
-        }
-      }).map((val) => {
-          return (
-        <SearchRecipe onChange={event => {setSearch(event.target.value)}} />
-          );
-      })}
-          
-        </div> */}
-      
       </div>
       <CreateRecipe />
     </div>
   );
 }
-
-
 
 export default App;
--- client/src/components/RecipePreview.js
+++ client/src/components/RecipePreview.js
@@ -1,13 +1,29 @@
 import React from "react";
 import { useState, useEffect } from "react";
 
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+
 export default function RecipePreview(props) {
-  const { title, prep_time, portion_size, image } = props.recipe || {};
+  const { id, title, prep_time, portion_size, image } = props.recipe || {};
+
+  const onClick = () => {
+    props.onRecipeClick(props.recipe);
+  };
 
   console.log("props", props.recipe);
   // empty object
   return (
-    <div className="">
+
+    // <BrowserRouter>
+    //   <nav>
+    //   <Link to="/about">About</Link>
+    //   <br />
+    // </nav>
+    // </BrowserRouter>
+
+
+
+    <div onClick={onClick} className="">
       <div className=" bg-sunset rounded-[20px] w-60 p-10 m-5 outline outline-offset-2outline-charcoal shadow-[8px_8px_#AE574D]">
         <div className="flex justify-center">
           <div className="flex-wrap">
