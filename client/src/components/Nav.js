49b1f5c67914090ad783576dc40c0e5875aeb925
icoolww <williamwijaya.mail@gmail.com>
2022-06-17 00:07:19 +0000

    Adding Tasty Eats anchor tag to homepage

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -6,7 +6,9 @@ export default function Nav(props) {
     <>
     <nav className="flex justify-between">
       <h1 className="text-sunset text-4xl font-bold p-10">
+        <a href={"/"}>
         Tasty Eats.
+        </a>
       </h1>
       <SearchRecipe {...props} />
    </nav>
