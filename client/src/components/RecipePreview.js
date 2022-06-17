5bf89e6e22cfd2775e2a4b3933e5b5bfbbd98eeb
Kelly Atmore <kelly.atmore47@gmail.com>
2022-06-16 17:56:14 -0700

    homepage styling -- add icons

--- client/src/components/RecipePreview.js
+++ client/src/components/RecipePreview.js
@@ -13,11 +13,11 @@ export default function RecipePreview(props) {
   const catName = (category_id) => {
     let name = "";
     if (category_id === 1) {
-      name = "breakfast";
+      name = "Breakfast";
     } else if (category_id === 2) {
-      name = "lunch"
+      name = "Lunch"
     } else {
-      name = "dinner"
+      name = "Dinner"
     }
     return name
   }
@@ -37,7 +37,7 @@ export default function RecipePreview(props) {
   // empty object
   return (
     
-    <div onClick={onClick} className="m-8 p-5 hover:scale-105 hover:bg-[#D15E51] transition ease-in-out bg-sunset rounded-[20px] w-60 outline outline-offset-2outline-charcoal shadow-[8px_8px_#AE574D]">
+    <div onClick={onClick} className=" cursor-pointer m-8 pl-10 pr-10 pt-5 pb-5 hover:scale-105 hover:bg-[#D15E51] transition ease-in-out bg-sunset rounded-[20px] w-60 outline h-90 outline-offset-2outline-charcoal shadow-[8px_8px_#AE574D]">
         <FavRecipe onClick={handleFavRecipe} isFav={true} />
         {/* Need to change isFav with true/false to change Heart Icon */}
         <div className="flex justify-center">
@@ -48,13 +48,22 @@ export default function RecipePreview(props) {
           </div>
         </div>
         <h1 className="text-oatmeal font-medium">{title}</h1>
-        <p>{catName(category_id)}</p>
+        <p className="text-[#FCD6A8] text-sm pb-5">{catName(category_id)}</p>
 
-        
-          <p className="text-oatmeal text-sm">{prep_time}</p>
-          <p className="text-oatmeal text-sm ">{portion_size}</p>
+      <div className="recipe_details flex">
+      <img className="icon" src="../icons/stopwatch.png" />
+     <p className="text-oatmeal text-sm">{prep_time}</p>
+      </div>
+          
+          
+          <div className="flex">
+          <img className="icon" src="../icons/user.png" />
+       <p className="text-oatmeal text-sm ">
+        {portion_size}</p>
         </div>
-    
+        </div>
+        
+   
    
 
   );
